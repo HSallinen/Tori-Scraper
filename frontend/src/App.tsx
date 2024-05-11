bfe88764b760bb52b09b405248e3c6854c2a2b34
Hermanni <sallinen.hermanni@gmail.com>
2024-05-11 19:03:17 +0300

    add input interface

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,34 +1,25 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import styles from './App.module.css'
+import {TextField, Typography} from '@mui/material'
+import InputSlider from './InputSlider'
+import { Button } from '@mui/base'
+import CityInput from './CityInput'
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [inputtedItem, setInputtedItem] = useState("")
+  const [priceRange, setPriceRange] = useState([50, 100])
+  const [city, setCity] = useState<string|undefined>()
+  const [distance, setDistance] = useState(50)
+  const[email, setEmail] = useState("")
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className={styles.app}>
+      <TextField label="mitä tuotetta haluat etsiä?" value={inputtedItem} onChange={(event) => {setInputtedItem(event.target.value)}} required/>
+      
+      <InputSlider name='Hinta(€)' value={priceRange} setValue={setPriceRange} min={0} max={4000}  valueLabelDisplay="on" marks={[{value: 100, label: "100€"}, {value: 500, label: "500€"}, {value:1000, label:"1000€"}, {value: 2000, label: "2000€"}, {value:3000, label:"3000€"}, {value:4000, label:"4000€"}]}/>
+      <CityInput selectedCity={city} setCity={setCity}></CityInput>
+      <InputSlider name="Maksimi Etäisyys(km)" value={distance} setValue={setDistance} min={10} max={800} step={10}valueLabelDisplay="on" marks={[{value: 100, label: "100km"}, {value: 200, label:"200km"}, {value: 300, label:"300km"}, {value: 400, label:"400km"}, {value: 500, label:"500km"}, {value: 600, label:"600km"}, {value: 700, label:"700km"}, {value: 800, label:"800km"}]}/>
+      <TextField label="sähköpostiosoite" value={email} onChange={(event) => {setEmail(event.target.value)}} required/>
+      <Button onClick = {()=>{}}>Aloita haku</Button>
+    </div>
   )
 }
 
