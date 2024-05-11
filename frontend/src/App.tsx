8a4aab2396f3fe88ea9be5dbd772c59f110aa13f
Hermanni <sallinen.hermanni@gmail.com>
2024-05-11 20:26:22 +0300

    add expirery date picker

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,6 +4,10 @@ import {TextField, Typography} from '@mui/material'
 import InputSlider from './InputSlider'
 import { Button } from '@mui/base'
 import CityInput from './CityInput'
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider/LocalizationProvider'
+import { DatePicker } from '@mui/x-date-pickers/DatePicker';
+import 'dayjs/locale/fi';
 function App() {
   const [inputtedItem, setInputtedItem] = useState("")
   const [priceRange, setPriceRange] = useState([50, 100])
@@ -18,6 +22,10 @@ function App() {
       <CityInput selectedCity={city} setCity={setCity}></CityInput>
       <InputSlider name="Maksimi Etäisyys(km)" value={distance} setValue={setDistance} min={10} max={800} step={10}valueLabelDisplay="on" marks={[{value: 100, label: "100km"}, {value: 200, label:"200km"}, {value: 300, label:"300km"}, {value: 400, label:"400km"}, {value: 500, label:"500km"}, {value: 600, label:"600km"}, {value: 700, label:"700km"}, {value: 800, label:"800km"}]}/>
       <TextField label="sähköpostiosoite" value={email} onChange={(event) => {setEmail(event.target.value)}} required/>
+      <Typography>Valitse vanhentumispäivä</Typography>
+      <LocalizationProvider dateAdapter={AdapterDayjs} adapterLocale="fi">
+        <DatePicker />
+      </LocalizationProvider>
       <Button onClick = {()=>{}}>Aloita haku</Button>
     </div>
   )
