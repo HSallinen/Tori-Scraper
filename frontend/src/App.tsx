1c9a2d1bf5ea6162f60ee7107b48243d98a6e47f
Hermanni Sallinen <sallinen.hermanni@gmail.com>
2024-07-31 12:57:05 +0300

    add location picker

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -14,12 +14,22 @@ function App() {
   const [distance, setDistance] = useState(50)
   const[email, _setEmail] = useState("")
   const [data, setData] = useState<any[]>([])
+  const [_location, setLocation] = useState<GeolocationPosition>()
+  const getLocation = () => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition((location) => {console.log(location); setLocation(location)});
+    } 
+  }
+
   return (
     <div className={styles.app}>
       <TextField label="mitä tuotetta haluat etsiä?" value={inputtedItem} onChange={(event) => {setInputtedItem(event.target.value)}} required/>
       
       <InputSlider name='Hinta(€)' value={priceRange} setValue={setPriceRange} min={0} max={4000}  valueLabelDisplay="on" marks={[{value: 100, label: "100€"}, {value: 500, label: "500€"}, {value:1000, label:"1000€"}, {value: 2000, label: "2000€"}, {value:3000, label:"3000€"}, {value:4000, label:"4000€"}]}/>
-      <CityInput selectedCity={city} setCity={setCity}></CityInput>
+      <div className={styles.locationInput}>
+        <CityInput selectedCity={city} setCity={setCity}></CityInput>
+        <Button onClick={getLocation}>valitse siainti</Button>
+      </div>
       <InputSlider name="Maksimi Etäisyys(km)" value={distance} setValue={setDistance} min={10} max={800} step={10}valueLabelDisplay="on" marks={[{value: 100, label: "100km"}, {value: 200, label:"200km"}, {value: 300, label:"300km"}, {value: 400, label:"400km"}, {value: 500, label:"500km"}, {value: 600, label:"600km"}, {value: 700, label:"700km"}, {value: 800, label:"800km"}]}/>
 
       <Button onClick = {async()=>{setData(await makeRequest({product: inputtedItem, priceMin: Math.min(...priceRange), priceMax: Math.max(...priceRange), city: city||"PLACEHOLDER", distance, email, data: []}))}}>Aloita haku</Button>
